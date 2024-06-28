e5be53843002c6c57be3c27516f7e7e7a0b16015
Thanh Luan <thanhluando25@gmail.com>
2024-06-28 15:45:40 +0700

    Làm phần Features

---
+++ .history/assets/js/index_20240626144714.js
@@ -0,0 +1,6 @@
+var myVideo1 = document.getElementById("myVideo1");
+
+function playPause() {
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+}
---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+var myVideo1 = document.getElementById("myVideo1");
+
+function playPause() {
+    if (myVideo1.paused) myVideo1.play();
+    else myVideo1.pause();
+}
